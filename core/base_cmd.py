ff0e7e5cef2fb46c9bf55c5a67d39a9cecbb5f91
4e6anenk0 <serhiichebanenko@yahoo.com>
2023-01-13 21:33:14 +0200

    modified BaseCmd class

--- core/base_cmd.py
+++ core/base_cmd.py
@@ -5,23 +5,40 @@ import shlex
 
 class BaseCmd:
     def __init__(self):
-        self.result_exec = None
-    
-    def _cmd(self, cmd: str, password = None, root = None, timeout = 30):
-        print(password)
+        self.output_exec = None
+
+    def _get_sudo_str_list_cmd(self, cmd: str):
+        sudo_cmd = ['sudo', '-S']
+        cmd = shlex.split(cmd)
+        sudo_cmd.extend(cmd)
+        return sudo_cmd
+
+    def _subprocess_handler(self, result: subprocess.Popen[bytes]):
+        # повертає typle[bytes, bytes] з виводом та помилками, попередньо обробив результати
+        output, error = result.communicate()
+
+        if output:
+            print(
+                f"All done! Returned code is: {result.returncode}")
+        if error:
+            print(f"Returned code is: {result.returncode}")
+            print(f"Error: {error.decode('utf-8').strip()}")
+
+        return output, error
+
+    def _cmd(self, cmd: str, password=None, root=None, timeout=30):
+
         try:
             if root:
                 if password:
-                    sudo_cmd = ['sudo', '-S']
-                    command = shlex.split(cmd)
-                    sudo_cmd.extend(command)                  
-                    proc = subprocess.Popen(['echo', password], stdout=subprocess.PIPE)
-                    
+                    sub_proc = subprocess.Popen(
+                        ['echo', password], stdout=subprocess.PIPE)
+
                     return subprocess.Popen(
-                    sudo_cmd,
-                    stdout=subprocess.PIPE,
-                    stderr=subprocess.PIPE,
-                    stdin=proc.stdout
+                        self._get_sudo_str_list_cmd(cmd),
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE,
+                        stdin=sub_proc.stdout
                     )
                 else:
                     print("Password is empty!")
@@ -34,29 +51,13 @@ class BaseCmd:
                     stderr=subprocess.PIPE,
                     stdin=subprocess.PIPE
                 )
-                
-        except FileNotFoundError as ex:
-            print(f"Command [{cmd}] failed because the executable could not be found.\n{ex}")
-        except subprocess.CalledProcessError as ex:
+
+        except OSError as e:
             print(
-                f"Command [{cmd}] failed because did not return a successful return code. "
-                f"Returned {ex.returncode}\n{ex}"
-            )
-        except subprocess.TimeoutExpired as ex:
-            print(f"Command [{cmd}] timed out.\n{ex}")
+                f"OSError: {e.errno} \n OSError: {e.strerror} \n OSError: {e.filename}")
 
-    def root_run(self, cmd: str, password: str):
-        result = self._cmd(cmd, password, root=True)
-        if result == None:
-            print('Result is None!')
-            return
-        self.result_exec = result.stdout.read().decode('utf-8')
-        print(self.result_exec)
-
-    def run(self, cmd: str):
-        result = self._cmd(cmd)
-        if result == None:
-            print('Result is None!')
-            return
-        self.result_exec = result.stdout.read().decode('utf-8')
-        print(self.result_exec)
+    def run(self, cmd: str, password=None, root=False):
+        result = self._cmd(cmd, password, root)
+        output, error = self._subprocess_handler(result=result)
+        self.output_exec = output.decode('utf-8')
+        print(self.output_exec)
--- script.py
+++ script.py
@@ -16,8 +16,23 @@ system = System()
 cmd = BaseCmd()
 #cmd.run('ls -a /')
 #cmd.root.run('cp -rf /usr/share/themes/. /home/$USER/test', password='7991')
-cmd.root_run('ls -l', password='7991')
+cmd.run('ls -l', password='7991', root=True)
 cmd.run('ls -l')
+
+""" 
+
+cmd.chained_run('ls -a', 'grep venv')
+
+cnf.run('ls -a').run('grep venv')
+cnf.install('package')
+cnf.flatpak.install('package_id')
+cnf.snap.install('package')
+cnf.add_extension('extension_url')
+cnf.add_env(key = key, value = value)
+cnf.apply()
+
+"""
+
 '''
 conf = Configurator()
 conf.install()
